5aeab229498f66c69d4df76e6eb3c30bf8cbaf40
David Robb <66834033+drobb2020@users.noreply.github.com>
2022-09-07 14:29:10 -0400

    End of part 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import { useState } from 'react'
 
+// Custom Components
+import CustomForm from './components/CustomForm'
+import TaskList from './components/TaskList'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([])
+
+  const addTask = (task) => {
+    setTasks((prevState) => [...prevState, task])
+  }
+
+  const deleteTask = (id) => {
+    setTasks((prevState) => prevState.filter((t) => t.id !== id))
+  }
 
   return (
-    <div className="App">
-
+    <div className="container">
+      <header>
+        <h1>My Task List</h1>
+      </header>
+      <CustomForm addTask={addTask} />
+      {tasks && (
+        <TaskList 
+          tasks={tasks}
+          deleteTask={deleteTask}
+        />)}
     </div>
   )
 }
---
+++ src/components/CustomForm.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+// Library Imports
+import { PlusIcon } from '@heroicons/react/24/solid'
+
+const CustomForm = ({ addTask }) => {
+  const [task, setTask] = useState('')
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    addTask({
+        name: task,
+        checked: false,
+        id: Date.now(),
+    })
+    setTask('')
+  }
+
+  return (
+    <form className="todo" onSubmit={handleFormSubmit}>
+      <div className="wrapper">
+        <input
+          type="text"
+          id="task"
+          className="input"
+          value={task}
+          onInput={(e) => setTask(e.target.value)}
+          required
+          autoFocus
+          maxLength={60}
+          placeholder="Enter Task"
+        />
+        <label htmlFor="task" className="label">
+          Enter Task
+        </label>
+      </div>
+      <button className="btn" aria-label="Add Task" type="submit">
+        <PlusIcon className="h-6 w-6 text-blue-500" />
+      </button>
+    </form>
+  )
+}
+
+export default CustomForm
